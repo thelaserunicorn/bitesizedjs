7eb04405cc182a210169e13b7dbf8e9a77cf8c14
Rashid <rashideram2@gmail.com>
2020-11-26 12:16:26 +0600

    tictactoe

--- tictactoe_minimax/app.js
+++ tictactoe_minimax/app.js
@@ -1,16 +1,34 @@
-var board = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
+/*Code of Minmax here*/
+
+const board = [
+	[0, 0, 0],
+	[0, 0, 0],
+	[0, 0, 0],
 ];
 
-var HUMAN = -1;
-var COMP = +1;
+const HUMAN = -1;
+const COMP = +1;
 
-/* state = current board state */
-function gameOver(state, player){
-    var win_state = [
-        [state[0][0], state[0][1], state[0][2]],
+/* Function to heuristic evaluation of state. */
+function evalute(state) {
+	let score = 0;
+
+	if (gameOver(state, COMP)) {
+		score = +1;
+	}
+	else if (gameOver(state, HUMAN)) {
+		score = -1;
+	} else {
+		score = 0;
+	}
+
+	return score;
+}
+
+/* This function tests if a specific player wins */
+function gameOver(state, player) {
+	let win_state = [
+		[state[0][0], state[0][1], state[0][2]],
 		[state[1][0], state[1][1], state[1][2]],
 		[state[2][0], state[2][1], state[2][2]],
 		[state[0][0], state[1][0], state[2][0]],
@@ -18,28 +36,30 @@ function gameOver(state, player){
 		[state[0][2], state[1][2], state[2][2]],
 		[state[0][0], state[1][1], state[2][2]],
 		[state[2][0], state[1][1], state[0][2]],
-    ];
-    for (var i=0; i<8; i++){
-        var line = win_state[i];
-        var filled = 0;
-        for (var j=0; j < 3; j++){
-            if (line[j] == player){
-                filled++;
-            }
-        }
-        if (filled = 3){
-            return true;
-        }
-    }
+	];
+
+	for (let i = 0; i < 8; i++) {
+		let line = win_state[i];
+		let filled = 0;
+		for (let j = 0; j < 3; j++) {
+			if (line[j] == player)
+				filled++;
+		}
+		if (filled == 3)
+			return true;
+	}
+	return false;
 }
+
+/* This function test if the human or computer wins */
 function gameOverAll(state) {
 	return gameOver(state, HUMAN) || gameOver(state, COMP);
 }
 
 function emptyCells(state) {
-	var cells = [];
-	for (var x = 0; x < 3; x++) {
-		for (var y = 0; y < 3; y++) {
+	let cells = [];
+	for (let x = 0; x < 3; x++) {
+		for (let y = 0; y < 3; y++) {
 			if (state[x][y] == 0)
 				cells.push([x, y]);
 		}
@@ -48,34 +68,169 @@ function emptyCells(state) {
 	return cells;
 }
 
-function validMove(x, y){
-    var empties = emptyCells(board);
-    try{
-        if (board[x][y]==0){
-            return true;
-        }
-        else{
-            return false;
-        }
-        
-    } catch(e){
-        return false;
-    }
- }
+/* A move is valid if the chosen cell is empty */
+function validMove(x, y) {
+	let empties = emptyCells(board);
+	try {
+		if (board[x][y] == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	} catch (e) {
+		return false;
+	}
+}
 
+/* Set the move on board, if the coordinates are valid */
+function setMove(x, y, player) {
+	if (validMove(x, y)) {
+		board[x][y] = player;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
+/* *** AI function that choice the best move *** */
+// Read more on https://github.com/Cledersonbc/tic-tac-toe-minimax/
+function minimax(state, depth, player) {
+	let best;
 
+	if (player == COMP) {
+		best = [-1, -1, -1000];
+	}
+	else {
+		best = [-1, -1, +1000];
+	}
 
-function evaluate(state){
-    var score = 0;
-    if (gameOver(state, COMP)){
-        score = +1;
-    }
-    else if (gameOver(state, HUMAN)){
-        score = -1;
-    }
-    else{
-        score = 0;
-    }
-    return score;
+	if (depth == 0 || gameOverAll(state)) {
+		let score = evalute(state);
+		return [-1, -1, score];
+	}
+
+	emptyCells(state).forEach(function (cell) {
+		let x = cell[0];
+		let y = cell[1];
+		state[x][y] = player;
+		let score = minimax(state, depth - 1, -player);
+		state[x][y] = 0;
+		score[0] = x;
+		score[1] = y;
+
+		if (player == COMP) {
+			if (score[2] > best[2])
+				best = score;
+		}
+		else {
+			if (score[2] < best[2])
+				best = score;
+		}
+	});
+
+	return best;
+}
+
+/* It calls the minimax function */
+function aiTurn() {
+	let x, y;
+	let move;
+	let cell;
+
+	if (emptyCells(board).length == 9) {
+		x = parseInt(Math.random() * 3);
+		y = parseInt(Math.random() * 3);
+	}
+	else {
+		move = minimax(board, emptyCells(board).length, COMP);
+		x = move[0];
+		y = move[1];
+	}
+
+	if (setMove(x, y, COMP)) {
+		cell = document.getElementById(String(x) + String(y));
+		cell.innerHTML = "O";
+	}
+}
+
+/* main */
+function clickedCell(cell) {
+	let button = document.getElementById("bnt-restart");
+	button.disabled = true;
+	let conditionToContinue = gameOverAll(board) == false && emptyCells(board).length > 0;
+
+	if (conditionToContinue == true) {
+		let x = cell.id.split("")[0];
+		let y = cell.id.split("")[1];
+		let move = setMove(x, y, HUMAN);
+		if (move == true) {
+			cell.innerHTML = "X";
+			if (conditionToContinue)
+				aiTurn();
+		}
+	}
+	if (gameOver(board, COMP)) {
+		let lines;
+		let cell;
+		let msg;
+
+		if (board[0][0] == 1 && board[0][1] == 1 && board[0][2] == 1)
+			lines = [[0, 0], [0, 1], [0, 2]];
+		else if (board[1][0] == 1 && board[1][1] == 1 && board[1][2] == 1)
+			lines = [[1, 0], [1, 1], [1, 2]];
+		else if (board[2][0] == 1 && board[2][1] == 1 && board[2][2] == 1)
+			lines = [[2, 0], [2, 1], [2, 2]];
+		else if (board[0][0] == 1 && board[1][0] == 1 && board[2][0] == 1)
+			lines = [[0, 0], [1, 0], [2, 0]];
+		else if (board[0][1] == 1 && board[1][1] == 1 && board[2][1] == 1)
+			lines = [[0, 1], [1, 1], [2, 1]];
+		else if (board[0][2] == 1 && board[1][2] == 1 && board[2][2] == 1)
+			lines = [[0, 2], [1, 2], [2, 2]];
+		else if (board[0][0] == 1 && board[1][1] == 1 && board[2][2] == 1)
+			lines = [[0, 0], [1, 1], [2, 2]];
+		else if (board[2][0] == 1 && board[1][1] == 1 && board[0][2] == 1)
+			lines = [[2, 0], [1, 1], [0, 2]];
+
+		for (let i = 0; i < lines.length; i++) {
+			cell = document.getElementById(String(lines[i][0]) + String(lines[i][1]));
+			cell.style.color = "red";
+		}
+
+		msg = document.getElementById("message");
+		msg.innerHTML = "You lose!";
+	}
+	if (emptyCells(board).length == 0 && !gameOverAll(board)) {
+		let msg = document.getElementById("message");
+		msg.innerHTML = "Draw!";
+	}
+	if (gameOverAll(board) == true || emptyCells(board).length == 0) {
+		button.value = "Restart";
+		button.disabled = false;
+	}
+}
+
+/* Restart the game*/
+function restartBnt(button) {
+	if (button.value == "Start AI") {
+		aiTurn();
+		button.disabled = true;
+	}
+	else if (button.value == "Restart") {
+		let htmlBoard;
+		let msg;
+
+		for (let x = 0; x < 3; x++) {
+			for (let y = 0; y < 3; y++) {
+				board[x][y] = 0;
+				htmlBoard = document.getElementById(String(x) + String(y));
+				htmlBoard.style.color = "#444";
+				htmlBoard.innerHTML = "";
+			}
+		}
+		button.value = "Start AI";
+		msg = document.getElementById("message");
+		msg.innerHTML = "";
+	}
 }
